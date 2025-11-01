a3600e2fc7a1dbb4681c55e1acb3762e2eea815f
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-01 11:06:30 -0400

    Reorganize, add caching evaluator, have backfill pick up model from registry

--- pkn/ccflow/common/backfill.py
+++ pkn/ccflow/common/backfill.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from typing import Generic, List, Literal, Tuple, Type, TypeVar, Union
 
 from ccflow import (
-    BaseModel,
     CallableModel,
     CallableModelGenericType,
     ContextBase,
@@ -18,9 +17,9 @@ from ccflow import (
 from numpy import datetime64
 from pydantic import Field, PrivateAttr, SerializeAsAny, model_validator
 
+from .interval import Interval
+
 __all__ = (
-    "Offset",
-    "Interval",
     "BackfillContext",
     "BackfillModel",
 )
@@ -28,55 +27,6 @@ __all__ = (
 
 C = TypeVar("C", bound=ContextBase)
 R = TypeVar("R", bound=ResultBase)
-Offset = Literal[
-    "B",  # business day frequency
-    "C",  # custom business day frequency
-    "D",  # calendar day frequency
-    "W",  # weekly frequency
-    "ME",  # month end frequency
-    "SME",  # semi-month end frequency (15th and end of month)
-    "BME",  # business month end frequency
-    "CBME",  # custom business month end frequency
-    "MS",  # month start frequency
-    "SMS",  # semi-month start frequency (1st and 15th)
-    "BMS",  # business month start frequency
-    "CBMS",  # custom business month start frequency
-    "QE",  # quarter end frequency
-    "BQE",  # business quarter end frequency
-    "QS",  # quarter start frequency
-    "BQS",  # business quarter start frequency
-    "YE",  # year end frequency
-    "BYE",  # business year end frequency
-    "YS",  # year start frequency
-    "BYS",  # business year start frequency
-    "h",  # hourly frequency
-    "bh",  # business hour frequency
-    "cbh",  # custom business hour frequency
-    "min",  # minutely frequency
-    "s",  # secondly frequency
-    "ms",  # milliseconds
-    "us",  # microseconds
-    "ns",  # nanoseconds
-]
-
-
-class Interval(BaseModel):
-    offset: Offset
-    n: int = 1
-
-    @model_validator(mode="before")
-    @classmethod
-    def validate_n(cls, v, info):
-        if isinstance(v, str):
-            # v can be of form: "{n}{offset}", e.g. "15D"
-            # Split into n and offset
-            for i, char in enumerate(v):
-                if not char.isdigit():
-                    n = int(v[:i])
-                    offset = v[i:]
-                    return Interval(offset=offset, n=n)
-            raise ValueError(f"Invalid interval string: {v}")
-        return v
 
 
 class BackfillContext(DatetimeRangeContext, Generic[C]):
@@ -144,10 +94,12 @@ class BackfillModel(CallableModel, Generic[C, R]):
             contexts.append(context.context.model_copy(update={"datetime": step, "dt": step, "date": step.date()}))
         self._steps = contexts
         return [(self.model, contexts)]
-        # return [(self.model, [c]) for c in contexts]
 
     @Flow.call
     def __call__(self, context: BackfillContext[C]) -> R:
         result = {}
         print("This should happen after all calls and be cached")
+        print("You should not see any further executes if using caching evaluator!")
+        for step in self._steps:
+            self.model(context=step)
         return BackfillResult(value=result)
