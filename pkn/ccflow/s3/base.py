8206342ec7c95ec5e7d33467cc31e58444d6a083
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-01 19:04:15 -0400

    Tweak backfill config name, work on generic s3

--- pkn/ccflow/s3/base.py
+++ pkn/ccflow/s3/base.py
@@ -1,5 +1,5 @@
 from gzip import decompress
-from typing import List, Literal, TypeVar, Union
+from typing import List, Literal, Optional, TypeVar, Union
 
 from boto3 import Session
 from botocore.config import Config
@@ -16,6 +16,8 @@ __all__ = (
     "S3Config",
     "S3Session",
     "S3Client",
+    "S3Context",
+    "S3Result",
     "S3Model",
 )
 
@@ -58,34 +60,62 @@ class S3Client(BaseModel):
         )
 
 
-class S3Context(NullContext): ...
+class S3Context(NullContext):
+    bucket: Optional[str] = None
+    object: Optional[str] = None
+    # TODO: format?
+
+
+class S3ReadContext(S3Context): ...
+
+
+class S3WriteContext(S3Context): ...
+
+
+class S3WriteDataContext(S3WriteContext):
+    data: Union[bytes, str, dict]
+
+
+class S3WriteFileContext(S3WriteContext):
+    file: str
+
+
+class S3ReadWriteContext(S3Context):
+    read: S3ReadContext
+    write: S3WriteContext
 
 
 class S3Result(GenericResult): ...
 
 
+class S3ReadResult(S3Result): ...
+
+
+class S3WriteResult(S3Result): ...
+
+
+class S3ReadWriteResult(S3Result):
+    read: S3ReadResult
+    write: S3WriteResult
+
+
 class S3Model(CallableModelGenericType[S3Context, S3Result]):
-    bucket_name: str
-    object_key: str
+    bucket: Optional[str] = None
+    object: Optional[str] = None
     client: S3Client
 
     mode: Literal["read", "write", "read_write"] = "read"
     format: Union[ResultFormat, List[ResultFormat]] = "binary"
 
     def template(self) -> Template:
-        # Loads object_key as a Jinja2 template
-        return Environment().from_string(self.object_key)
+        # Loads object as a Jinja2 template
+        return Environment().from_string(self.object)
 
-    @Flow.call
-    def __call__(self, context: S3Context) -> S3Result:
-        # TODO: write/readwrite
-        # TODO: specify retry policy
-        # Use the S3 client to get the object from S3
-        s3_client = self.client.client
-        response = s3_client.get_object(Bucket=self.bucket_name, Key=self.object_key)
+    def _read_data(self, client: S3Client, bucket: str, object: str) -> S3ReadResult:
+        read_response = client.client.get_object(Bucket=bucket, Key=object)
 
         # Read as binary
-        data = response["Body"].read()
+        read_data = read_response["Body"].read()
 
         formats = [self.format] if not isinstance(self.format, list) else self.format
 
@@ -94,11 +124,41 @@ class S3Model(CallableModelGenericType[S3Context, S3Result]):
                 case "binary":
                     pass  # already binary
                 case "text":
-                    data = data.decode("utf-8")
+                    read_data = read_data.decode("utf-8")
                 case "json":
-                    data = loads(data)
+                    read_data = loads(read_data)
                 case "gzip":
-                    data = decompress(data)
+                    read_data = decompress(read_data)
                 case _:
                     raise ValueError(f"Unsupported result format: {format}")
-        return S3Result(value=data)
+        return S3ReadResult(value=read_data)
+
+    @Flow.call
+    def __call__(self, context: S3Context) -> S3Result:
+        # TODO: write/readwrite
+        # TODO: specify retry policy
+        # Use the S3 client to get the object from S3
+        bucket = context.bucket or self.bucket
+        object = context.object or self.object
+
+        if not bucket or not object:
+            raise ValueError("Both bucket and object must be specified either in the model or the context.")
+
+        if isinstance(context, S3ReadContext) or self.mode in ["read", "read_write"]:
+            read_result = self._read_data(self.client, bucket, object)
+        else:
+            read_result = None
+
+        if isinstance(context, S3WriteContext) or self.mode in ["write", "read_write"]:
+            write_result = None
+        else:
+            write_result = None
+
+        if read_result and write_result:
+            return S3ReadWriteResult(read=read_result, write=write_result)
+        elif read_result:
+            return read_result
+        elif write_result:
+            return write_result
+        else:
+            raise ValueError("No operation performed; check mode and context types.")
