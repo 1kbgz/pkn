8b4ff59350c679e2892a165a9da7159ac8fd880e
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-11-13 13:42:50 -0500

    Add importer utility, fixes #50

--- pkn/__init__.py
+++ pkn/__init__.py
@@ -1,33 +1,25 @@
 __version__ = "0.4.0"
 
+from .importer import make_lazy_getattr
 
-def _lazy_load(name):
-    import importlib
-
-    if name in (
-        "ccflow",
-        "infra",
-        "logging",
-        "pydantic",
-    ):
-        # import relative submodule
-        return importlib.import_module(f".{name}", package=__name__)
-    if name in (
-        "Message",
-        "SMTP",
-        "Attachment",
-        "Email",
-        "get_import_path",
-        "serialize_path_as_string",
-        "ImportPath",
-        "CallablePath",
-        "Dict",
-        "List",
-    ):
-        return importlib.import_module(".pydantic", package=__name__).__getattribute__(name)
-    if name in ("default", "getLogger", "getSimpleLogger"):
-        return importlib.import_module(".logging", package=__name__).__getattribute__(name)
-    raise AttributeError(f"module 'pkn' has no attribute '{name}'")
-
-
-__getattr__ = _lazy_load
+__getattr__ = make_lazy_getattr(
+    __name__,
+    {
+        "ccflow": [],
+        "infra": [],
+        "importer": ["make_lazy_getattr"],
+        "logging": ["default", "getLogger", "getSimpleLogger"],
+        "pydantic": [
+            "Message",
+            "SMTP",
+            "Attachment",
+            "Email",
+            "get_import_path",
+            "serialize_path_as_string",
+            "ImportPath",
+            "CallablePath",
+            "Dict",
+            "List",
+        ],
+    },
+)
